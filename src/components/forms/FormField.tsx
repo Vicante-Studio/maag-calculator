cb128f37f2cf126fe5832289cd6b3eaa9fb095af
Batubo Victory <victorybatubo76@gmail.com>
2026-08-19 15:28:31 +0100

    feat: Add value and objKey prop

--- src/components/forms/FormField.tsx
+++ src/components/forms/FormField.tsx
@@ -1,6 +1,6 @@
 import type { FormFieldProps } from "../../types/formTypes"
 
-const FormField = ({label, setFunction}: FormFieldProps) => {
+const FormField = ({label, objKey, value, propFunction}: FormFieldProps) => {
     return(
         <div className="mb-4">
             <label className="block text-sm font-medium text-gray-700 mb-1">
@@ -8,9 +8,10 @@ const FormField = ({label, setFunction}: FormFieldProps) => {
             </label>
             <input 
                 type="number" 
+                value={value || 0}
                 className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-red-500"
                 placeholder="0"
-                onChange={(e) => setFunction(Number(e.target.value))}
+                onChange={(e) => propFunction((prev) => ({...prev, [objKey]: Number(e.target.value) || 0}))}
             />
         </div>
     )
