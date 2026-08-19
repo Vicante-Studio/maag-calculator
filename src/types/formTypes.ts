d8da4fb80321c5469099a51a86c82243d9f5e443
Batubo Victory <victorybatubo76@gmail.com>
2026-08-19 15:27:27 +0100

    feat: Add additional types in the FormFieldProps interface

--- src/types/formTypes.ts
+++ src/types/formTypes.ts
@@ -1,6 +1,20 @@
 import type { Dispatch, SetStateAction } from "react";
 
+export interface AttendanceData {
+  men: number
+  women: number
+  teens: number
+  children: number
+}
+
 export interface FormFieldProps {
-    label:string,
-    setFunction: Dispatch<SetStateAction<number>>
+    label:string
+    propFunction: Dispatch<SetStateAction<AttendanceData>>
+    objKey: string,
+    value: number
+}
+
+export interface AttendanceFormProps {
+    weekNumber:number
+    updateWeek: (data: AttendanceData) => void
 }
